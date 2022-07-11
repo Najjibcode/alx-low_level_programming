ba19d0241fa8dad4309db80bdd128c1ab0dcaf1d
Najjibcode <jibzaj@gmail>
2022-07-11 07:49:55 -0700

    jiy

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,25 +8,25 @@
 void rev_string(char *s)
 
 {
-		int i;
-		int j;
-		int temp;
-		int length;
+	int i;
+	int j;
+	int temp;
+	int length;
 
-		while (s[i] != 0)
-		{
-		i++;
-		}
-		length = i;
-		i = 0;
-		j = length - 1;
-		while (i < j)
-		{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
-		i++;
-		j--;
-		}
+	while (s[i] != 0)
+	{
+	i++;
+	}
+	length = i;
+	i = 0;
+	j = length - 1;
+	while (i < j)
+	{
+	temp = s[i];
+	s[i] = s[j];
+	s[j] = temp;
+	i++;
+	j--;
+	}
 
 }
