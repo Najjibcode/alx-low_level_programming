1b9f4d4868e481c9029e11afcda60ddb7292de3f
Najjibcode <jibzaj@gmail>
2022-07-22 05:24:13 -0700

    fit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,85 +1,43 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
 
-
-
-
-
+#include <stdlib.h>
 
 /**
- * coin_count - counts the min coins needed for change
- *
- * @argv: argument vector
- * @count: int to count coins
- * Return: int
+ *main-prints the minimum number of coins
+ *to make change for an amount of money.
+ *@argc:number of commandline arguments.
+ *@argv:pointer to an array of commmand line arguments.
+ *Return:0-success, non-zero-fail.
  */
-int coin_count(char *argv[], int count)
-{
-int sum = 0;
-
-while (!(sum + 25 > atoi(argv[1])))
-{
-sum += 25;
-count++;
-}
-while (!(sum + 10 > atoi(argv[1])))
-{
-sum += 10;
-count++;
-}
-while (!(sum + 5 > atoi(argv[1])))
-{
-sum += 5;
-count++;
-}
-while (!(sum + 2 > atoi(argv[1])))
-{
-sum += 2;
-count++;
-}
-while (!(sum + 1 > atoi(argv[1])))
-{
-sum += 1;
-count++;
-}
-return (count);
-}
-
-
-
-
-
-
-
-
-/**
- * main - prints the min num of coins to make change for an amount of money
- *
- * @argc: argument count
- * @argv: argument vector
- * Return: int
- */
-
-
 
 
 
 int main(int argc, char *argv[])
-{
-int count = 0;
 
-if (argc != 2)
+{
+
+if (argc == 2)
+{
+int i, leastcents = 0, money = atoi(argv[1]);
+int cents[] = {25, 10, 5, 2, 1};
+for (i = 0; i < 5; i++)
+{
+if (money >= cents[i])
+{
+leastcents += money / cents[i];
+money = money % cents[i];
+if (money % cents[i] == 0)
+{
+break;
+}
+}
+}
+printf("%d\n", leastcents);
+}
+else
 {
 printf("Error\n");
 return (1);
 }
-else if (atoi(argv[1]) < 0)
-{
-printf("%d\n", 0);
-return (0);
-}
-count = coin_count(argv, count);
-printf("%d\n", count);
 return (0);
 }
