dd4e91aa894068664983fda5a4ee5f054cb04f5d
NajjibCode <jibzaj@gmail.com>
2022-10-03 06:57:36 -0700

    Libz

--- 0x18-dynamic_libraries/calc.c
+++ 0x18-dynamic_libraries/calc.c
@@ -10,19 +10,15 @@ int sub(int a, int b)
 
 int mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
 
 int div(int a, int b)
 {
-	if (b != 0)
-		return (a / b);
-	return (0);
+	return (a / b);
 }
 
 int mod(int a, int b)
 {
-	if (b != 0)
-		return (a % b);
-	return (0);
+	return (a % b);
 }
